dc323092b91d6e5b832283181b67a105da66f47a
Marcelino Fuentes <marcelinofuentes@gmail.com>
2025-02-25 12:38:32 +0100

    Update

--- python/modules/parse_args.py
+++ python/modules/parse_args.py
@@ -1,16 +1,35 @@
 """Parses command-line arguments."""
 
 import argparse
+import os
+
+
+def get_available_projects():
+    """Return a list of available projects."""
+
+    return [d for d in os.listdir("projects") if os.path.isdir(os.path.join("projects", d))]
+
+
+def get_available_layouts():
+    """Return a list of available layouts."""
+
+    return [
+        os.path.splitext(d)[0]
+        for d in os.listdir("layouts")
+        if os.path.isfile(os.path.join("layouts", d))
+    ]
 
 
 def parse_args():
     """Parse command-line arguments."""
 
+    available_projects = get_available_projects()
+    available_layouts = get_available_layouts()
+    project_help = "Options: {}".format(", ".join(available_projects))
+    layout_help = "Options: {}".format(", ".join(available_layouts))
+
     parser = argparse.ArgumentParser()
-    parser.add_argument(
-        "--project", type=str, help="Project name (e.g., hamilton, mgnr)"
-    )
-    parser.add_argument(
-        "--layout", type=str, required=True, help="Layout name (e.g., basic, unit)"
-    )
+    parser.add_argument("--project", type=str, help=project_help)
+    parser.add_argument("--layout", type=str, required=True, help=layout_help)
+
     return parser.parse_args()
