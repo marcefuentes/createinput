ced4ebe73641bc71a0a31871c8fce1711a70b515
Marcelino Fuentes <marcelinofuentes@gmail.com>
2025-03-23 12:46:48 +0100

    Update

--- python/main.py
+++ python/main.py
@@ -2,8 +2,6 @@
 
 """Create input files for simulations."""
 
-from collections.abc import Callable
-
 from modules.common_generator import common_generator
 from modules.parse_args import parse_args
 from modules.project_loader import get_module_attribute
@@ -14,13 +12,9 @@ def main():
 
     args = parse_args()
     print(f"Projetc: {args.project}, layout: {args.layout}")
-    generator = get_module_attribute(
-        f"projects.{args.project}.generator", "generator", Callable
-    )
-    settings = get_module_attribute(
-        f"projects.{args.project}.settings", "settings", dict
-    )
-    layout = get_module_attribute(f"layouts.{args.layout}", "layout", dict)
+    generator = get_module_attribute(f"projects.{args.project}.generator", "generator")
+    settings = get_module_attribute(f"projects.{args.project}.settings", "settings")
+    layout = get_module_attribute(f"layouts.{args.layout}", "layout")
 
     common_generator(generator, settings, layout)
 
--- python/modules/project_loader.py
+++ python/modules/project_loader.py
@@ -4,10 +4,6 @@ from importlib import import_module
 from pathlib import Path
 
 
-class DiscoveryError(Exception):
-    """Raised when a module or attribute cannot be found."""
-
-
 def detect_project(project=None):
     """Detects all projects in the projects directory."""
 
@@ -30,40 +26,31 @@ def detect_project(project=None):
 
 def get_available_projects():
     """Return a list of available projects."""
+
     projects_path = Path(__file__).resolve().parent.parent / "projects"
     return [d.name for d in projects_path.iterdir() if d.is_dir()]
 
 
 def get_available_layouts():
     """Return a list of available layouts."""
+
     layouts_path = Path(__file__).resolve().parent.parent / "layouts"
     return [d.stem for d in layouts_path.iterdir() if d.is_file()]
 
 
-def get_module_attribute(module_path, attr_name, expected_type=None):
+def get_module_attribute(module_path, attr_name):
     """Dynamically imports a module and retrieves the specified attribute.
 
     Args:
         module_path (str): Module path to import.
         attr_name (str): Attribute name to retrieve.
-        expected_type (type, optional): Expected type of the attribute.
 
     Returns:
-        The requested attribute if found and valid.
-
-    Raises:
-        DiscoveryError: If the module or attribute is missing or has the wrong type.
+        The requested attribute if found.
     """
-    try:
-        module = import_module(module_path)
-    except ModuleNotFoundError as exc:
-        raise DiscoveryError(f"Module '{module_path}' not found.") from exc
+
+    module = import_module(module_path)
 
     attr = getattr(module, attr_name, None)
-
-    if expected_type and not isinstance(attr, expected_type):
-        raise DiscoveryError(
-            f"'{attr_name}' in '{module_path}' is not of type {expected_type.__name__}."
-        )
 
     return attr
