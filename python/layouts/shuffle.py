7fe87ffaf62b5c214139b956a9842b1f694bcac8
Marcelino Fuentes <marcelinofuentes@gmail.com>
2025-04-09 12:00:02 +0200

    Update

---
+++ python/layouts/shuffle.py
@@ -0,0 +1,12 @@
+"""Basic."""
+
+layout = {
+    "Language": [0],
+    "Shuffle": [1],
+    "Cost": [-3],
+    # "Cost,-15, # mgnr
+    "GroupSize": [7],
+    "mechanisms": ["none", "none", "d", "d", "p", "pd"],
+    "Given": [0.0, 1.0, 0.0, 1.0, 1.0, 1.0],
+    "Populations": [1, 2],
+}
