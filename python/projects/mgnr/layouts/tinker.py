dedf422fff0090d1a776397b98d40a4eecd64439
Marcelino Fuentes <marcelinofuentes@gmail.com>
2025-04-17 10:33:32 +0200

    Update

---
+++ python/projects/mgnr/layouts/tinker.py
@@ -0,0 +1,12 @@
+# Tinker
+
+from projects.mgnr.layouts.default import setup as default_setup
+from settings.layout import layout
+
+
+def setup():
+    """Setup the layout."""
+
+    default_setup()
+    layout["shuffles"] = [0, 1]
+    layout["mechanisms"] = ["i", "pi"]
